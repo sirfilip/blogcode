dc7039633dca41ece1fb40c5f8ddd4088b3b8256
Filip Kostovski <github.sirfilip@gmail.com>
2016-03-18 11:22:26 +0100

    Finish with prototyping

---
+++ proto.rb
@@ -0,0 +1,34 @@
+module Taskr
+
+  def task_repo 
+    @task_repo ||= {}
+  end
+  
+  def desc(task_description)
+    @task_description = task_description
+  end
+
+  def task(task_name, &task_action)
+    task_repo[task_name] = {
+      "action" => task_action,
+      "description" => @task_description || ""
+    }
+    # reset the description 
+    @task_description = ''
+  end
+
+  def run_task(task_name)
+    task_repo[task_name]['action'].call
+  end
+
+end
+extend Taskr
+
+desc "This is the first task"
+task :my_first_task do 
+  puts "This is my first task"
+end
+
+puts task_repo.inspect
+
+run_task :my_first_task
